51a1df264f52e8f29a4c427e5180bda9ea9d18bd
vihaga237 <134221757+vihaga237@users.noreply.github.com>
2023-05-21 23:52:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94741842307', 'sandaru', true],
+  ['94741842307', 'SL VIHAGA BOT', true], 
   [''] 
 ] //Number of owners
 
@@ -47,7 +47,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'SANDARU-YT'
+global.botname = 'VIHAGA'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
 global.author = '@Asliguru' 
